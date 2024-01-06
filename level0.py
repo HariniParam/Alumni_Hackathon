9f70ce4bd82eff400995a4f3833eb7988a3cb51a
unknown <21PT05@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 14:42:30 +0530

    Level 01

--- level0.py
+++ level0.py
@@ -15,29 +15,32 @@ vehicle_no = (list(vehicle_details)[0])
 
 for val in data['neighbourhoods']:
     graph[val] = data["neighbourhoods"][val]["distances"]
-
 #Function to find shortest path
 def shortestPath(start, curr, minVal):
-    if all(ele == 1 for ele in visited):
-        return
-    index = 0
-    minimum = graph[startNode][0]+minVal
-    startNeighbour = 'n'+str(index)
-    for each in range(len(graph[start])):
-        print(graph[start], minimum)
-        if visited[each]==0 and graph[startNode][each]!=0 and graph[startNode][each]+minVal < minimum:
-            startNeighbour = 'n'+str(each)
-            minimum = graph[startNode][each]+minVal
-            index = each
-    visited[index] = 1
-    curr.append(startNeighbour)
-    shortestPath(startNeighbour, curr, minimum)
+    for i in range(noOfNeighbour-1):
+        #print("Previous min ", minVal)
+        #print("start value ", start)
+        for each in range(len(visited)):
+            if visited[each] == 0 and (int(start[1:]) != each):
+                minimum = graph[start][each]+minVal
+                break
+        #startNeighbour = 'n'+str(index)
+        for each in range(len(graph[start])):
+            if visited[each]==0 and graph[start][each]!=0 and graph[start][each]+minVal <= minimum:
+                startNeighbour = 'n'+str(each)
+                minimum = graph[start][each]+minVal
+                index = each
+        visited[index] = 1
+        curr.append(startNeighbour)
+        start = startNeighbour
+        minVal=minimum
+        #shortestPath(startNeighbour, curr, minimum)
 
 #Starting from the restaurant
 startNode = vehicle_details[vehicle_no]['start_point']
-visited = [0]*noOfNeighbour
+visited = [0]*(noOfNeighbour)
 minVal = graph[startNode][0]
-index = 0
+#index = 0
 for each in range(len(graph[startNode])):
     if graph[startNode][each] < minVal:
         startNeighbour = 'n'+str(each)
@@ -49,14 +52,13 @@ curr.append(startNeighbour)
 shortestPath(startNeighbour, curr, minVal)
 curr.insert(0, startNode)
 curr.append(startNode)
-
+ 
 
 resDict = {}
 pathDict = {}
 pathDict["path"] = curr
 resDict[vehicle_no] = pathDict
 print(resDict)
-
 json_object = json.dumps(resDict)
 with open("C:\Alumni_Hackathon\Output_data\level0.json", "w") as outfile:
     outfile.write(json_object)
